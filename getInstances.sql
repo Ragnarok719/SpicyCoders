74c7746e0959fa9e9c1935d2c0393772898e1c51
Edmund Wong <w.edmund.hh@gmail.com>
2014-06-04 15:00:06 -0700

    Basic SQL Files
    - create tables
    - populate instances
    - get instances

---
+++ getInstances.sql
@@ -0,0 +1,38 @@
+SELECT *
+FROM Author;
+
+SELECT *
+FROM Book;
+
+SELECT *
+FROM BookType;
+
+SELECT *
+FROM Category;
+
+SELECT *
+FROM HasAuthor;
+
+SELECT *
+FROM HasPublisher;
+
+SELECT *
+FROM HasSearchGenre;
+
+SELECT *
+FROM Librarian;
+
+SELECT *
+FROM Patron;
+
+SELECT *
+FROM Publisher;
+
+SELECT *
+FROM SearchGenre;
+
+SELECT *
+FROM Checkout;
+
+SELECT *
+FROM Returns;
