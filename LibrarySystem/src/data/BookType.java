bff47ce03e795b070e30485c17bdcd1d0a045064
Alex G <lpzind@live.ca>
2014-06-13 14:40:34 -0700

    Added equals methods to book type and category for easier testing.

--- LibrarySystem/src/data/BookType.java
+++ LibrarySystem/src/data/BookType.java
@@ -24,5 +24,35 @@ public class BookType {
 	public void setOverdueFee(int overdueFee) {
 		this.overdueFee = overdueFee;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + maxReservation;
+		result = prime * result + overdueFee;
+		result = prime * result
+				+ ((typeName == null) ? 0 : typeName.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BookType other = (BookType) obj;
+		if (maxReservation != other.maxReservation)
+			return false;
+		if (overdueFee != other.overdueFee)
+			return false;
+		if (typeName == null) {
+			if (other.typeName != null)
+				return false;
+		} else if (!typeName.equals(other.typeName))
+			return false;
+		return true;
+	}
 	
 }
--- LibrarySystem/src/data/Category.java
+++ LibrarySystem/src/data/Category.java
@@ -23,6 +23,43 @@ public class Category {
 	}
 	public void setSuperCategoryId(Integer superCategoryId) {
 		this.superCategoryId = superCategoryId;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((idNumber == null) ? 0 : idNumber.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result
+				+ ((superCategoryId == null) ? 0 : superCategoryId.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		if (idNumber == null) {
+			if (other.idNumber != null)
+				return false;
+		} else if (!idNumber.equals(other.idNumber))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (superCategoryId == null) {
+			if (other.superCategoryId != null)
+				return false;
+		} else if (!superCategoryId.equals(other.superCategoryId))
+			return false;
+		return true;
 	}	
 	
 }
